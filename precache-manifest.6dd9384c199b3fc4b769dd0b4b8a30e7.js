0525810eaa5306244d56c3443b79999b13ce3728
VBN2470 <vbn2470@gmail.com>
2022-02-27 19:23:48 +1100

    Updates

---
+++ precache-manifest.6dd9384c199b3fc4b769dd0b4b8a30e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3266b69f9bc1718e13c59f6bedecde6",
+    "url": "/golf/index.html"
+  },
+  {
+    "revision": "0c320d2aea87b6ac7bb7",
+    "url": "/golf/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "cf45024e1244623b3dd7",
+    "url": "/golf/static/css/main.422f18b9.chunk.css"
+  },
+  {
+    "revision": "0c320d2aea87b6ac7bb7",
+    "url": "/golf/static/js/2.11675d47.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/golf/static/js/2.11675d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf45024e1244623b3dd7",
+    "url": "/golf/static/js/main.7d480727.chunk.js"
+  },
+  {
+    "revision": "5ad8ee8b9367086f49a2",
+    "url": "/golf/static/js/runtime-main.2cb1606c.js"
+  },
+  {
+    "revision": "60ea48a501795d790493e3b38684b83e",
+    "url": "/golf/static/media/Course_10.60ea48a5.jpg"
+  },
+  {
+    "revision": "fa2a46f5fe7fc90c5d4aa5752aaf7b15",
+    "url": "/golf/static/media/Course_7.fa2a46f5.jpg"
+  },
+  {
+    "revision": "d4f1acc0118d6e388b4799cc6c87e695",
+    "url": "/golf/static/media/Course_8.d4f1acc0.jpg"
+  },
+  {
+    "revision": "9b0e152e67a3e86cbdf7a7a0f8e1fe0f",
+    "url": "/golf/static/media/js.9b0e152e.jpg"
+  },
+  {
+    "revision": "a569e4521e001cd6ee2d159304174b15",
+    "url": "/golf/static/media/logo.a569e452.png"
+  },
+  {
+    "revision": "0395e62c2fa841468eca2f50413474d5",
+    "url": "/golf/static/media/tee.0395e62c.PNG"
+  }
+]);
